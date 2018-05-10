5043925a3dd1d94e0df2e7d5f7dd5f40cdd8c959
xuchangyue <xcyue@mail.ustc.edu.cn>
2018-05-10 15:59:33 +0800

    Add files via upload

---
+++ lambda_log_filter.py
@@ -0,0 +1,33 @@
+import re
+import boto3
+
+internalIp = ['54.222.11.143']
+
+s3_client = boto3.client('s3')
+
+def logfilter(file_path, filtered_path):
+    f = open(file_path, 'r')
+    w = open(filtered_path, 'w')
+    patternip = '|'.join(internalIp)
+    pattern = '.*('+patternip+').*'
+    print(pattern)
+    for line in f.readlines():
+        if not re.match(pattern, line):
+            w.write(line)
+    f.close()
+    w.close()
+
+def lambda_handler(event, context):
+
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        download_path = '/tmp/{}'.format(key)
+        upload_path = '/tmp/filtered-{}'.format(key)
+
+        s3_client.download_file(bucket, key, download_path)
+        logfilter(download_path, upload_path)
+        
+        s3_client.upload_file(upload_path,
+             '{}-filtered'.format(bucket),
+             'filtered-{}'.format(key))
